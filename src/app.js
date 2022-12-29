d485fdbde9543154aac4eb605005ef9ac9308d35
Victoria <victoria1998castellar@hotmail.com>
2022-12-29 12:01:23 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,59 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  let symbols = ["$", "%", "!", "=", "&"];
+  let uppercase = [
+    "A",
+    "V",
+    "C",
+    "D",
+    "E",
+    "B",
+    "R",
+    "T",
+    "U",
+    "M",
+    "P",
+    "O",
+    "I",
+    "Y",
+    "L",
+    "W",
+    "Q",
+    "Z",
+    "X",
+    "H",
+    "U",
+    "K",
+    "G",
+    "S"
+  ];
+  let lowercase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "x",
+    "y",
+    "z",
+    "r",
+    "s"
+  ];
+  let placeForPassword = (document.querySelector(
+    ".placeForPassword"
+  ).innerHTML = numbers[2]);
 };
